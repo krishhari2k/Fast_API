691d22114180a2806cdb62aa62a6179e9f697b57
krishhari2k19 <krishhari2k19@gmail.com>
2025-05-10 12:33:45 +0530

    1.15

--- fast_api/main.py
+++ fast_api/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
 app = FastAPI()
 
-
 @app.get("/")
 def read_root():
-    return {"message": "Hello, FastAPI!"}
-
+    return {"message": "hello world"}
 
 @app.get("/posts")
 def get_posts():
     return {"data": "This is a list of posts"}
+
+
+
+# @app.post("/createpost")
+# def create_post(payload: dict = Body(...)):
+#     return {"message": "Post created successfully", "data": payload}
